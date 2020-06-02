29219650944dbeea68f40bc6dfa8364094c9fa03
ura148 <yuki.ura10@gmail.com>
2020-06-02 17:09:34 +0900

    add： 魚の種類別のポイントを表示するbtnを追加 釣果数の入力エリアを追加 種類別ポイント＊釣果数のポイント計算btnを追加 釣果をさらに記録するためのbtn追加 全体の釣果数を表示するbtnを追加

---
+++ addTemplate.js
@@ -0,0 +1,58 @@
+// ボタンを押したらtemplateを追加
+function addTemplate(){
+  // template要素を取得
+  let template = document.getElementById('template');
+  // template要素の内容を複製
+      clone = template.content.cloneNode(true);
+
+  // div#containerの中に追加
+  document.getElementById('container').appendChild(clone);
+}
+
+
+function onSumCountButtonClick(){
+  let countElements = document.getElementsByClassName("count");
+
+  console.log(countElements);
+
+  let result = 0
+
+  for(let i = 0; i < countElements.length; i++){
+    result += eval(countElements[i].value);
+  }
+
+  let resultP = document.getElementById("count");
+
+  resultP.textContent = "今日の合計釣果は" + result + "匹です";
+}
+
+function displaypoint() {
+  let pointP = document.getElementById("fishPoint");
+      fishPoint = document.fish.fishName;
+      num = fishPoint.selectedIndex;
+      pointValue = fishPoint.options[num].value;
+
+      if(pointValue == 0){
+        pointP.textContent = "魚を選んでください"
+      }else{
+        pointP.textContent = "その魚は" + pointValue + "pointです";
+      }
+}
+
+function displayFishPointCount(){
+  let count = document.getElementsByClassName("count");
+      result = 0;
+
+  for(let i = 0; i < count.length; i++){
+    result += eval(count[i].value);
+  }
+
+  let pointP = document.getElementById("fishpoint");
+      fishPoint = document.fish.fishName;
+      num = fishPoint.selectedIndex;
+      pointValue = fishPoint.options[num].value;
+      pointP = document.getElementById("point")
+      fishPointCount = result * pointValue;
+
+  pointP.textContent = fishPointCount + "points獲得";
+}
